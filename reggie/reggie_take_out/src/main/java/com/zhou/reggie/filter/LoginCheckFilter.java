8ab418da1cc091157e89d323e917cad51879e8f2
ZDy <975313032@qq.com>
2023-04-09 00:10:38 +0800

    2023/4/9

--- reggie/reggie_take_out/src/main/java/com/zhou/reggie/filter/LoginCheckFilter.java
+++ reggie/reggie_take_out/src/main/java/com/zhou/reggie/filter/LoginCheckFilter.java
@@ -61,7 +61,6 @@ public class LoginCheckFilter implements Filter {
         //4.1判断登录状态，如果已经登录，则放行(pc端)
         if(request.getSession().getAttribute("employee") != null){
             log.info("用户已登录，用户id为{}",request.getSession().getAttribute("employee"));
-
             //获取当前登录用户的id并存到维护的ThreadLocal线程空间中去
             Long empId = (Long)request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
